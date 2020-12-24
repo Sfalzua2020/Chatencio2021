861118b6ef582fc6610430601d6ea21fb01c3125
gustavoghp87 <ghp.2120@gmail.com>
2020-12-24 11:15:52 -0300

    server

--- src/controllers/database.js
+++ src/controllers/database.js
@@ -2,7 +2,6 @@ const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
 const url = process.env.ATLAS_DB
-const database = 'chatencio'
 
 const client = new MongoClient(url,
     {
